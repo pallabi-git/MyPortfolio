0a0c9dd8af456d782dee4782f7605d09174f706a
pallabi-git <duttapallabiwb@gmail.com>
2026-02-03 17:16:36 +0530

    add latest resume

--- Portfolio-web/src/App.jsx
+++ Portfolio-web/src/App.jsx
@@ -13,8 +13,8 @@ function App() {
       <Hero />
       <About />
       <Experience />
-      {/* <Projects /> */}
-      <Contact />
+      {/* <Projects /> 
+      <Contact />*/}
     </div>
   );
 }
